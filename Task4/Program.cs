4b867ee87c3d4a17a9ee343aaa2024c2f7eca507
patrickreillyUU <Reilly-P6@ulster.ac.uk>
2025-09-29 16:29:54 +0100

    Add project files.

---
+++ Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿void Task4()
+{
+    int year;
+    Console.WriteLine("Please enter a year:");
+    year = Convert.ToInt32(Console.ReadLine());
+
+    if (year % 4 == 0)
+    {
+        Console.WriteLine("You have entered a leap year.");
+    }
+    else if (year % 100 == 0) 
+    {
+        Console.WriteLine("You have not entered a leap year.");
+    }
+    else if (year % 400  == 0) 
+    {
+         Console.WriteLine("You have entered a leap year.");
+    }
+    else if (year < 0)
+    {
+        Console.WriteLine("Invalid year.");
+    }
+    else
+    {
+        Console.WriteLine("You have not entered a leap year.");
+    }
+
+}
+
+Task4();
