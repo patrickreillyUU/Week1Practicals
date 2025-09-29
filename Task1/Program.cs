9091e48f97959de93bf0bb9f086e405b82d6ad0d
Patrick Reilly <Reilly-P6@ulster.ac.uk>
2025-09-29 14:50:30 +0100

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿void Task1()
+{
+    string name;
+    int age;
+    Console.WriteLine("Please enter your name:");
+    name = Console.ReadLine();
+
+    Console.WriteLine("Please enter your age:");
+    age = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"Hello {name}, you will be {age + 5} in 5 years");
+
+
+}
+Task1();
+
+
+
